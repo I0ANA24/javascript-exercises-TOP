a01714724e78d4440c8e31af95e981cb8a351ef9
I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2024-05-26 19:40:13 +0300

    ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = (x, y) => {
+    let sum = 0;
+    if(!Number.isInteger(x) || !Number.isInteger(y))
+        return "ERROR";
+    if(x < 0 || y < 0)
+        return "ERROR";
+    if(x > y)
+        [x, y] = [y, x];
 
-};
+    for(let i = x; i <= y; i++)
+        sum += i;
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
